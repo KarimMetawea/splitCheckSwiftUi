1cebea2e84a6c93998d185a063be77f1319d5f7f
Karim Metawea <ks.metawea@mohre.gov.ae>
2021-09-16 11:55:46 +0400

    Initial Commit

---
+++ Shared/SwiftUIFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirstProjectApp.swift
+//  Shared
+//
+//  Created by Karem on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
