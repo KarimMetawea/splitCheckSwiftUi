09c196b97c136f3328f0a72e16be6215e288b1a3
Karim Metawea <ks.metawea@mohre.gov.ae>
2021-09-19 09:32:52 +0400

    finished split check app

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 2
+    
+    
+    let tipPercentages = [10,15,20,25,0]
+    var totalPerPerson:Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let amount = Double(checkAmount) ?? 0
+        let totalAmount = amount + ((Double(tipPercentages[tipPercentage]) * 0.01) * amount)
+        return totalAmount / peopleCount
+    }
+   
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        Form{
+            Section{
+                TextField("Check Amount", text: $checkAmount).keyboardType(.decimalPad)
+                    
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach (2..<100){
+                        Text("\($0) people")
+                    }
+                }
+            }
+        
+            Section(header:Text("How much tip do you want to leave?")){
+                
+                Picker("Tip percentage", selection: $tipPercentage) {
+                    ForEach (0..<tipPercentages.count){
+                        Text("\(self.tipPercentages[$0]) %")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header:Text("Amount Per Person")){
+                Text("$ \(totalPerPerson)")
+            }
+            
+        }.navigationTitle("We Split")
+        }
     }
 }
 
